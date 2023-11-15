6b3f46833cc0b75f7fbf02a2c99dba6a902d36e1
Quentin Tellier <QTELLIER@3KLES.local>
2023-11-15 16:52:21 +0100

    Update angular version to 17

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,12 @@ import { LOCALE_ID, NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import { KlesMaterialDynamicformsModule } from 'kles-material-dynamicforms';
 import { MaterialModule } from './modules/material.module';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { KlesNgPipeModule} from '@3kles/kles-ng-pipe';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import localeFr from '@angular/common/locales/fr';
 import { CommonModule, registerLocaleData } from '@angular/common';
-import { SelectTriggerComponent } from './select/select-trigger.component';
-import { SelectOptionComponent } from './select/select-option.component';
+
 registerLocaleData(localeFr);
 @NgModule({
   declarations: [
@@ -26,17 +21,7 @@ registerLocaleData(localeFr);
     AppRoutingModule,
     MaterialModule,
     BrowserAnimationsModule,
-    FlexLayoutModule,
-    HttpClientModule,
-    TranslateModule.forRoot(
-      {
-        loader: {
-          provide: TranslateLoader,
-          useFactory: (HttpLoaderFactory),
-          deps: [HttpClient]
-        }
-      }
-    )
+    HttpClientModule
   ],
   bootstrap: [AppComponent]
 })

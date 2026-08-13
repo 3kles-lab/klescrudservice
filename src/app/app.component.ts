1efe8bc01bf2596dd370b44cfe1da19f78ce90da
QTELLIER <q.tellier@3kles-consulting.com>
2026-08-13 11:59:14 +0200

    chore: Update example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 
+import { CommonModule } from '@angular/common';
 import { Component, ViewEncapsulation } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    encapsulation: ViewEncapsulation.None,
-    standalone: false
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterOutlet,
+  ]
 })
-export class AppComponent{
+export class AppComponent {
   title = 'KlesCRUDService';
 }
